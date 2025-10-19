7fa9c35554a608716e671e2b6a6d56b1458ba891
AosZ <40927300+AAosZ@users.noreply.github.com>
2025-10-19 02:47:15 -0400

    Update Snake.py

--- 2023/Personal/Snake.py
+++ 2023/Personal/Snake.py
@@ -1,43 +1,44 @@
-DIRECTIONS = {
-    'UP': (0, 1),
-    'RIGHT': (1, 0),
-    'DOWN': (0, -1),
-    'LEFT': (-1, 0),
-}
+UP = (0, -1)
+RIGHT = (1, 0)
+DOWN = (0, 1)
+LEFT = (-1, 0)
 
-class Snake():
-    def __init__(self, initialize_snake, initialize_direction):
-        self.initialize_snake = initialize_snake
-        self.initialize_direction = initialize_direction
+
+class Snake:
+    def __init__(self, snakebody, direction):
+        self.snakebody = snakebody
+        self.direction = direction
 
     def take_step(self, position):
-        self.take_step = self.initialize_snake[1:] + [position]
+        for part in self.snakebody:
+            part[0] += position[0]
+            part[1] += position[1]
 
     def set_direction(self, direction):
-        self.set_direction = direction
+        self.direction = direction
 
     def head(self):
-        return self.initialize_snake[-1]
+        return self.snakebody[-1]
 
 
-class Apple():
+class Apple:
     pass
 
-class Game():
+
+class Game:
     def __init__(self, height, width):
         self.width = width
         self.height = height
-        self.snake = Snake([(0, 0), (1, 0), (2, 0), (3, 0)], 'UP')
-
+        self.snake = Snake([[0, 0], [1, 0], [2, 0], [3, 0]], 'RIGHT')
 
     def board(self):
         board = []
         for i in range(self.height):
             row = []
             for j in range(self.width):
-                if (j, i) == self.snake.head():
+                if [j, i] == self.snake.head():
                     row.append('X')
-                elif (j, i) in self.snake.initialize_snake:
+                elif [j, i] in self.snake.snakebody:
                     row.append('O')
                 else:
                     row.append(' ')
@@ -55,8 +56,7 @@ class Game():
             for j in range(self.width):
                 row.append(board[i][j])
 
-
-        print('+', end = '')
+        print('+', end='')
         for iii in range(self.width):
             print('-', end='')
         print('+', end='\n')
@@ -67,33 +67,63 @@ class Game():
                 print(board[ii][iii], end='')
             print('|')
 
-        print('+', end = '')
+        print('+', end='')
         for iii in range(self.width):
             print('-', end='')
         print('+', end='')
 
+
+def check_valid_move(body, x, y):
+    for position in body:
+        if position[0] == x and position[1] == y:
+            return False
+    return True
+
+# DIRECTIONS ARE X,Y (row, col) (height, width)
+# create something that detects collision with itself
 if __name__ == "__main__":
-    game = Game(10, 20)
+    game = Game(10, 30)
     game.render()
+    # print(head[0] + UP[0]) debug
     while True:
         d = input("\nSnake direction: ")
-        for i in range(game.height):
-            for j in range(game.width):
-                if i < 0 or i >= game.height or j < 0 or j >= game.width:
-                    if d == 'w':
-                        game.snake.set_direction('UP')
-                        game.snake.take_step((0, 1))
-                        game.render()
-                    if d == 'd':
-                        game.snake.set_direction('RIGHT')
-                        game.snake.take_step((1, 0))
-                        game.render()
-                    if d == 's':
-                        game.snake.set_direction('DOWN')
-                        game.snake.take_step((0, -1))
-                        game.render()
-                    if d == 'a':
-                        game.snake.set_direction('LEFT')
-                        game.snake.take_step((-1, 0))
-                        game.render()
-                print("Invalid move.")
+        head = game.snake.head()
+        if d == 'w':
+            if head[0] + UP[0] >= 0 and head[1] + UP[1] >= 0 and head[0] + UP[0] <= game.height and head[1] + UP[1] <= game.width:
+                if not check_valid_move(game.snake.snakebody, head[0] + UP[0], head[1] + UP[1]):
+                    print("Invalid move.")
+                    continue
+                game.snake.set_direction('UP')
+                print(head[0] + UP[0])
+                print(head[1] + UP[1])
+                game.snake.take_step(UP)
+                game.render()
+        elif d == 'd':
+            if head[0] + RIGHT[0] >= 0 and head[1] + RIGHT[1] >= 0 and head[0] + RIGHT[0] <= game.height and head[1] + RIGHT[1] <= game.width:
+                if not check_valid_move(game.snake.snakebody, head[0] + RIGHT[0], head[1] + RIGHT[1]):
+                    print("Invalid move.")
+                    continue
+                print(head[0] + RIGHT[0])
+                game.snake.set_direction('RIGHT')
+                game.snake.take_step(RIGHT)
+                game.render()
+        elif d == 's':
+            if head[0] + DOWN[0] >= 0 and head[1] + DOWN[1] >= 0 and head[0] + DOWN[0] <= game.height and head[1] + DOWN[1] <= game.width:
+                if not check_valid_move(game.snake.snakebody, head[0] + DOWN[0], head[1] + DOWN[1]):
+                    print("Invalid move.")
+                    continue
+                game.snake.set_direction('DOWN')
+                game.snake.take_step(DOWN)
+                game.render()
+        elif d == 'a':
+            if head[0] + LEFT[0] >= 0 and head[1] + LEFT[1] >= 0 and head[0] + LEFT[0] <= game.height and head[1] + LEFT[1] <= game.width:
+                if not check_valid_move(game.snake.snakebody, head[0] + LEFT[0], head[1] + LEFT[1]):
+                    print("Invalid move.")
+                    continue
+                print(head[0] + LEFT[0])
+                print(head[1] + LEFT[1])
+                game.snake.set_direction('LEFT')
+                game.snake.take_step(LEFT)
+                game.render()
+        else:
+            print("Invalid move.")
